932ad1fa6cce091a3367ad5a5085ad3cbfe8f518
talha_d59 <talha_d59@users.noreply.github.com>
2025-12-04 00:00:22 +0500

    Remove local env/build from repo; add .gitignore and CI for frontend/backend build & deploy

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Talha/mini-compiler-cpp
-# Build directory: C:/Users/Talha/mini-compiler-cpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/jsoncpp")
